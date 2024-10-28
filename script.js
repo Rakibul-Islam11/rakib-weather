58d04978d679fb68c388230d1c814ad5fbb8637e
rakib al muqtadir <kathgolap909@gmail.com>
2024-10-29 01:36:00 +0600

    fj

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 'use strict'
-const catchimggOne = document.getElementById("imggOne");
+const catchimggOne = document.getElementById("imggOne"),
+    catchsrcbox = document.getElementById("srcbox"),
+    catchbtn = document.getElementById("btn"),
+    catcherrorOutput = document.getElementById('errorOutput')
 
+//bg function
 function updateBgImg() {
-    if ( window.innerWidth <= 600 ) {
-        catchimggOne.src = "media/mobile-bg.mp4"
+    if (window.innerWidth <= 600) {
+        catchimggOne.src = "media/mobile-bg.mp4";
     } else {
-        catchimggOne.src = "media/pc-bg.mp4"
+        catchimggOne.src = "media/pc-bg.mp4";
     }
 }
 
-window.addEventListener('resize', function () {
-    updateBgImg()
-})
+window.addEventListener('resize', updateBgImg);
+//inptbox validation
+function inptValidation() {
+    let getInptValue = catchsrcbox.value;
+    if (getInptValue === '') {
+        alert("Searchbox can not be empty");
+    } else if (!isNaN(getInptValue)) {
+        alert("Please type city name with alphabetic letters");
+    } else {
+        resAPi(getInptValue);
+    }
+}
+
+//weather api
+const apiKey = "4bf7034f65250d979e8e842ed16200fe";
+function resAPi(recgetInptValue) {
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${recgetInptValue}&appid=${apiKey}`;
+    fetch(apiURL)
+        .then(res => res.json())
+        .then(data => {
+            if (data.cod === "404") {
+                catcherrorOutput.innerHTML = `<h2>${data.message}</h2>`
+            } else {
+                weatherWork(data)
+            }
+        })
+        .catch(error => console.error("Network Error:", error.message)); // নেটওয়ার্ক জনিত কোনো সমস্যা হলে এটিও দেখাবে
+}
+
+function weatherWork(recdata) {
+    let recieveApiData = recdata;
+
+}
+
+
+// বাটনে ক্লিক করলে `inptValidation` ফাংশন কল হবে
+catchbtn.addEventListener('click', inptValidation);
