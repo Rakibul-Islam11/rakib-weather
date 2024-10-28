d2ea4bc879abcdaefc3e5291e34f47205327c69e
rakib al muqtadir <kathgolap909@gmail.com>
2024-10-28 23:14:27 +0600

    fni

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-// 'use strict'
-// const catchimggOne = document.getElementById("imggOne");
+'use strict'
+const catchimggOne = document.getElementById("imggOne");
 
-// function updateBgImg() {
-//     if ( window.innerWidth <= 600 ) {
-//         catchimggOne.src = "media/mobile-bg.mp4"
-//     } else {
-//         catchimggOne.src = "media/pc-bg.mp4"
-//     }
-// }
+function updateBgImg() {
+    if ( window.innerWidth <= 600 ) {
+        catchimggOne.src = "media/mobile-bg.mp4"
+    } else {
+        catchimggOne.src = "media/pc-bg.mp4"
+    }
+}
 
-// // ডিবাউন্স ফাংশন 
-// function debounce(func, delay) {
-//     let timer;
-//     return function () {
-//         clearTimeout(timer);
-//         timer = setTimeout(func, delay);
-//     };
-// }
-
-// updateBgImg()
-// // window.addEventListener('resize', function () {
-// //     updateBgImg()
-// // })
-// window.addEventListener("resize", debounce(updateBgImg, 200));
+window.addEventListener('resize', function () {
+    updateBgImg()
+})
