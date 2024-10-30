bfe708e678bfee8077b90cdb639d4410a93f1b2d
rakib al muqtadir <kathgolap909@gmail.com>
2024-10-30 12:19:15 +0600

    mfiji

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 'use strict'
+
+
+
+
 const catchimggOne = document.getElementById("imggOne"),
     catchsrcbox = document.getElementById("srcbox"),
     catchbtn = document.getElementById("btn"),
-    catcherrorOutput = document.getElementById('errorOutput')
+    catcherrorOutput = document.getElementById('errorOutput'),
+    catchcelcius = document.getElementById("celcius"),
+    catchsunrise = document.getElementById("sunrise"),
+    catchsunset = document.getElementById("sunset"),
+    catchcityName = document.getElementById('cityName'),
+    catchvisibility = document.getElementById("visibility"),
+    catchweather = document.getElementById('weather'),
+    catchwind = document.getElementById('wind'),
+    catchinfoMain = document.getElementById('infoMain'),
+    catchpowerBy = document.getElementById('powerBy')
 
 
 //inptbox validation
@@ -25,8 +38,20 @@ function resAPi(recgetInptValue) {
         .then(res => res.json())
         .then(data => {
             if (data.cod === "404") {
+                
                 catcherrorOutput.innerHTML = `<h2>${data.message}</h2>`
+                // Clear previous data
+                catchcityName.innerHTML = '';
+                catchcelcius.innerHTML = '';
+                catchsunrise.innerHTML = '';
+                catchsunset.innerHTML = '';
+                catchvisibility.innerHTML = '';
+                catchweather.innerHTML = '';
+                catchwind.innerHTML = '';
+                catchpowerBy.innerHTML = '';
             } else {
+                catcherrorOutput.innerHTML = '';
+                
                 weatherWork(data)
             }
         })
@@ -35,6 +60,63 @@ function resAPi(recgetInptValue) {
 
 function weatherWork(recdata) {
     let recieveApiData = recdata;
+    console.log(recieveApiData)
+    catcherrorOutput.innerHTML = '';
+    //cloud
+    catchweather.innerHTML = `<h1>Weather: ${recieveApiData.weather[0].main} [Type - ${recieveApiData.weather[0].description}]</h1>`
+    function convertTo() {
+        catchcityName.innerHTML = `<h1>${recieveApiData.name}</h1>`
+        //cecius con
+        let getTemp = recieveApiData.main.temp;
+        let defcel = (getTemp - 273.15).toFixed(2) + "°C";
+        catchcelcius.innerHTML = `<h1>Temperature : ${defcel} </h1>`
+        
+        //sunrise and sunset con
+        let sunrise = recieveApiData.sys.sunrise;
+        let getTimeSunrise = new Date(sunrise * 1000);
+        catchsunrise.innerHTML = `<h1>Sunrise : ${getTimeSunrise.toLocaleTimeString()}</h1>`
+        //sunset
+        let sunset = recieveApiData.sys.sunset;
+        let getTimeSunset = new Date(sunset * 1000);
+        catchsunset.innerHTML = `<h1>Sunset : ${getTimeSunset.toLocaleTimeString()}</h1>`
+
+        //visibility con
+        let visi = recieveApiData.visibility;
+        let conVisi = visi / 1000
+        catchvisibility.innerHTML = `<h1>Visibility : ${conVisi} km </h1>`
+
+        //wind
+        function wind() {
+            let windd = recieveApiData.wind.deg;
+            let direction;
+            if (windd >= 0 && wind.deg <= 45) {
+                direction = "North-East";
+            } else if (windd > 45 && wind.deg <= 135) {
+                direction = "East";
+            } else if (windd > 135 && wind.deg <= 225) {
+                direction = "South";
+            } else if (windd > 225 && wind.deg <= 315) {
+                direction = "West";
+            } else {
+                direction = "North-West";
+            }
+            //speed
+            let spedd = recieveApiData.wind.speed;
+            let conspeedd = (spedd * 3.6).toFixed(2)
+            //gust
+            let gustt = recieveApiData.wind.gust;
+            let congustt = (gustt * 3.6).toFixed(2)
+            console.log(congustt);
+            
+            
+            
+            catchwind.innerHTML = `<h1>Wind Speed : ${conspeedd}km/h <br>----Direction: ${direction} </h1>`
+        }
+        catchpowerBy.innerHTML = `<h5>Powerd by <span class="text-red-500">RAKIB</span></h5>`
+        wind()
+    }
+    convertTo()
+    
 
 }
 
